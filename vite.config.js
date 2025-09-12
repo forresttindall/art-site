7b92049d4866fc8aa1a14d5942f276ba2e2d056a
forresttindall <forrest.tindall@gmail.com>
2025-09-12 17:34:28 -0600

    projects updates

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,10 @@ import react from '@vitejs/plugin-react';
 
 export default {
   plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  },
+  preview: {
+    historyApiFallback: true,
+  },
 };
