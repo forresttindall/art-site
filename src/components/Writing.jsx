80d4ec6477e7bb949b72f659c8aca87c384ee272
forresttindall <forrest.tindall@gmail.com>
2025-09-19 19:02:05 -0600

    projects updates

--- src/components/Writing.jsx
+++ src/components/Writing.jsx
@@ -5,29 +5,59 @@ const Writing = () => {
     <div className="page-content">
       <h1>Writing</h1>
 
-     <div className="featured-image">
+
+
+     {/* Second article: On Purpose */}
+      <p className="mono-text">
+        <span className="bold">PROJECT:</span> On Purpose
+      </p>
+      <p className="mono-text">
+        <span className="bold">DATE:</span> 9.19.2025
+      </p>
+      <br />
+      <p>The cultural expectation of life is a clear linear path. A path not even chosen by you, but by generations of pragmatists and worse yet, dentists. You're expected to go to school, then another, then another, followed by the only real school that actually mattered. You're then expected to find a career, and dedicate your whole life to it. You're expected to get married and have children. You are now successful, congratulations. You may now be happy.</p>
+      <br />
+      <p>We are expected to find a singular fixed purpose and spend our lives dedicated to this purpose. This leaves many feeling dissatisfied, paralyzed or trapped. Did you really dream of being an accountant? I do not think so.</p>
+      <br />
+      <p>What if your purpose was not to follow some grand plan, but to simply live and evolve? To follow your dreams as they evolve.</p>
+      <br />
+      <p>Society pushes a notion of fixed goals, a set plan, a clear outline for your life that must be followed. If you do not have a grand plan by the age of 18 you now are falling behind. You must “find your purpose” as if it is something to locate. Did you lose it? Maybe purpose can be found in motion, in adaptation and change. Perhaps it isn’t about chasing a life plan at all, but about staying open to the ever-shifting journey of living.</p>
+      <br />
+      <p>What if life was not about arriving at a fixed destination, but is about growth, change, becoming and evolving. Biological life would not even exist without massive evolution and change. Biological life and our lives are designed to change. All of the greatest minds of history did not have a fixed purpose, Aristotle, Benjamin Franklin, Isaac Newton, and Leonardo da Vinci all allowed themselves to change and evolve over their lives. All of them didn't have a single career, a single focus, a single purpose, but many.</p>
+      <br />
+      <p>Life is less like following a map and more like catching trains as they come: uncertain, romantic, sometimes risky, but alive. The beauty lies in not always knowing the destination. It forces you to stay present, to savor the scenery while it lasts. Allowing your purpose to evolve forces you to be resilient in a world of easy mac and next day delivery. It creates openness. Openness to change, to experience, and to possibility. For the sake of argument let's say you achieve your plan. You have become that hedge fund manager with kids that you set out to be. Now what? Your plan is done. Where is your purpose now? Have you lost it?</p>
+      <br />
+      <p>What if the only goal for your evolving life was to not settle? To frame figuring it out as a creative act. Like an artist molds a sculpture, what if the act of shaping a life was less about finding a finished form and more about molding as you go? Like clay that never fully hardens, your purpose is whatever you are becoming in the act of creating it. Maybe the point isn’t to have it figured out at all. Maybe the point is to keep moving, keep asking, and keep allowing yourself to change.</p>
+      <br />
+      <br />
+
+
+      {/* First article */}
+      <div className="featured-image">
         <div className="writing-image">
-        <img src="/images/_DSC1574.jpg" alt="Biography photo" />
+          <img src="/images/_DSC1574.jpg" alt="Biography photo" />
         </div>
       </div>
-            <p className="mono-text">
-            <span className="bold">PROJECT:</span> A Love Letter To The Snake River
-          </p>
-         <p className="mono-text">
-            <span className="bold">DATE:</span> 9.17.2025 
-          </p>
-          <p className="mono-text">
-            <span className="bold">Location:</span> 42.69206511381512, -115.6712428054687
-          </p>
-        <br></br>
-    <p>A tangerine sky blankets overhead, the wind whipping at my face carries the scent of sage and earth. At the canyon's edge the earth falls away without warning, a chasm cut clean by the mighty Snake River.</p>
-    <br></br>
-    <p>The Bruneau Canyon doesn't announce its presence so much as it reveals the sudden absence, the ground vanishing into an immense void of stone. The cliffs fall away into sheer walls, volcanic rock layered in rust, lichen. In morning light the walls are cool and blue: by midday they blaze like hammered copper. Sage and bitterbrush cling to the rim, stubborn threads of green in an endless barrenness. A red tailed hawk circles above the gorge, a lizard darts between fractured rocks at my feet. The scale is immense, yet it is the smallest movements, the shift of light, the flick of a tail, that remind you the canyon is alive.</p>
-    <br></br>
-    <p>The overlook offers a pause from everyday life. The Shoshone-Paiute people long ago used these lands, knowing the river, hiding in the cliffs, and harvesting the plants. Later ranchers followed the atlas of water and stone, grazing cattle across the plateau, drawing water from the Snake River, always aware of how the land demands respect. Today the overlook serves locals and travelers alike, a quiet destination, a place to breathe and consider. When I stood at the rim, the wind and the depth and the stillness pressed in. It was more than a view, it felt like an agreement: that some things are vast and enduring, unchanged by small concerns.</p>
-    <br></br>
-    <p>The Bruneau Canyon is patient in ways we cannot be. A chasm cut by a mighty river grain by grain, year after year, until stone gave way. To stand at its edge is to be reminded of humility, that our lives are short but still part of a larger rhythm. Out here the desert does not rush. It waits, quiet and enduring, holding a lesson as wide as the horizon itself.</p>
-    <br></br>
+      <p className="mono-text">
+        <span className="bold">PROJECT:</span> A Love Letter To The Snake River
+      </p>
+      <p className="mono-text">
+        <span className="bold">DATE:</span> 9.17.2025 
+      </p>
+      <p className="mono-text">
+        <span className="bold">Location:</span> 42.69206511381512, -115.6712428054687
+      </p>
+      <br />
+      <p>A tangerine sky blankets overhead, the wind whipping at my face carries the scent of sage and earth. At the canyon's edge the earth falls away without warning, a chasm cut clean by the mighty Snake River.</p>
+      <br />
+      <p>The Bruneau Canyon doesn't announce its presence so much as it reveals the sudden absence, the ground vanishing into an immense void of stone. The cliffs fall away into sheer walls, volcanic rock layered in rust, lichen. In morning light the walls are cool and blue: by midday they blaze like hammered copper. Sage and bitterbrush cling to the rim, stubborn threads of green in an endless barrenness. A red tailed hawk circles above the gorge, a lizard darts between fractured rocks at my feet. The scale is immense, yet it is the smallest movements, the shift of light, the flick of a tail, that remind you the canyon is alive.</p>
+      <br />
+      <p>The overlook offers a pause from everyday life. The Shoshone-Paiute people long ago used these lands, knowing the river, hiding in the cliffs, and harvesting the plants. Later ranchers followed the atlas of water and stone, grazing cattle across the plateau, drawing water from the Snake River, always aware of how the land demands respect. Today the overlook serves locals and travelers alike, a quiet destination, a place to breathe and consider. When I stood at the rim, the wind and the depth and the stillness pressed in. It was more than a view, it felt like an agreement: that some things are vast and enduring, unchanged by small concerns.</p>
+      <br />
+      <p>The Bruneau Canyon is patient in ways we cannot be. A chasm cut by a mighty river grain by grain, year after year, until stone gave way. To stand at its edge is to be reminded of humility, that our lives are short but still part of a larger rhythm. Out here the desert does not rush. It waits, quiet and enduring, holding a lesson as wide as the horizon itself.</p>
+      <br />
+
+ 
     </div>
   );
 };
