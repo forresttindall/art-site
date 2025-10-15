ff17ab85093679f932b3c570c5247b0a22ad14d1
forresttindall <forrest.tindall@gmail.com>
2025-10-15 17:30:44 -0600

    illustration update

--- src/components/Illustration.jsx
+++ src/components/Illustration.jsx
@@ -5,6 +5,21 @@ const Illustration = () => {
     <div className="homepage">
       <div className="homepage-content">
         <h1>Illustration</h1>
+
+         <div className='project'>
+          <div className="featured-image">
+            <img src="/images/nokings2.PNG" alt="Featured artwork" />
+          </div>
+          <p className="mono-text">
+            <span className="bold">Title:</span> No Kings No. 2
+          </p>
+          <p className="mono-text">
+            <span className="bold">medium:</span> Digital
+          </p>
+          <p className="mono-text">
+            <span className="bold">DATE:</span> 10.14.2025
+          </p>
+        </div>
         
     <div className='project'>
           <div className="featured-image">
