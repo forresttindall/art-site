a5a7108cbd117c3fb8c8645ea264341f81933822
forresttindall <forrest.tindall@gmail.com>
2025-10-17 14:37:49 -0600

    darkmode

--- src/components/Biography.jsx
+++ src/components/Biography.jsx
@@ -5,34 +5,88 @@ const Biography = () => {
     <div className="page-content">
       <h1>Biography</h1>
       
+
+     
+
+
       <div className="featured-image2">
         <div className="biography-image">
-        <img src="/images/IMG_2132.JPG" alt="Biography photo" />
+        <img src="/images/IMG_2094.JPG" alt="Biography photo" />
         </div>
       </div>
       
 <p className="mono-text">
-  J F Tindall is a multifaceted human born in Boise, Idaho and raised in the expansive landscapes of the American West. He began making art at an early age, first through drawing and writing, then discovering film photography at the age of 13. His work explores identity, society, philosophy and the stories we tell ourselves about belonging. Through photography, writing, illustration, and mixed media, he documents the quiet contradictions of contemporary life, the spaces between what we call things and what they actually are.
+  J F Tindall is a multifaceted artist born in Boise, Idaho and raised in the expansive landscapes of the American West. He began making art at an early age, first through drawing and writing, then discovering film photography at the age of 13. His work explores identity, society, and philosophy. Through photography, writing, illustration, and mixed media, he documents the quiet contradictions of contemporary life, the spaces between what we call things and what they actually are.
 </p>
 <br />
 <p className="mono-text">
   In 2013, Tindall launched <em>Tindall Knives</em>, beginning his career as a knifemaker. This practice of shaping steel by hand eventually drew him deeper into solitude and craft, leading him north into the mountains, where he lived as a hermit knifemaker for 10 years. That period of retreat became a crucible for patience, discipline, and attention to detail, qualities that continue to inform his creative practice today.
 </p>
+
 <br />
 <p className="mono-text">
-  His practice is rooted in observation and patience, finding meaning in the mundane and beauty in the overlooked. Whether capturing the stark honesty of small-town architecture or exploring what it means to be human, he is drawn to the gaps between expectation and experience.
+  Currently based in Mountain Home, Idaho, his work has been focused on documenting the unique landscape of a former boom town. His work has been featured in regional exhibitions and global publications, always seeking to bridge the distance between the personal and the universal.
 </p>
 <br />
-<p className="mono-text">
-  Currently based in Mountain Home, Idaho, he continues to investigate the relationship between landscape and language, using various media to question how we construct meaning from life. His work has been featured in regional exhibitions and global publications, always seeking to bridge the distance between the personal and the universal.
-</p>
-<br />
-<p className="mono-text">
-  When not creating, Tindall can be found wandering the west, collecting stories and coordinates, building the discipline of creation one frame, one word, one moment at a time.
-</p>
-<br />
+
+
+
+ <br></br>
+      <h1>Publications</h1>
+    
+        <div className='project'>
+           <div className="featured-image">
+            <img src="/images/IMG_3005.jpg" alt="Featured artwork" />
+            <img src="/images/IMG_3004.jpg" alt="Featured artwork" />
+          </div>
+          <p className="mono-text">
+            <span className="bold">Publication:</span> Popular Mechanics Magazine
+          </p>
+          <p className="mono-text">
+            <span className="bold">Date:</span> JULY/AUG 2017
+          </p>
+        </div>
+
+ <div className='project'>
+           <div className="featured-image">
+            <img src="/images/campfire.JPG" alt="Featured artwork" />
+          </div>
+          <p className="mono-text">
+            <span className="bold">Publication:</span> Sun Valley Magazine
+          </p>
+          <p className="mono-text">
+            <span className="bold">Date:</span> 9.23.2019
+          </p>
+        </div>
+
+ <div className='project'>
+           <div className="featured-image">
+            <img src="/images/IMG_8359.JPG" alt="Featured artwork" />
+          </div>
+          <p className="mono-text">
+            <span className="bold">Publication:</span> Condor Knife And Tool Catalog Cover
+          </p>
+          <p className="mono-text">
+            <span className="bold">Date:</span> 1.30.2017
+          </p>
+        </div>
+
+ <div className='project'>
+           <div className="featured-image">
+            <img src="/images/_DSC9182.jpg" alt="Featured artwork" />
+          </div>
+          <p className="mono-text">
+            <span className="bold">Publication:</span> SHOT Show Exhibit Wall
+          </p>
+          <p className="mono-text">
+            <span className="bold">Date:</span> 2016 & 2017
+          </p>
+        </div>
+
       </div>
     
+    
+
     
   );
 };
