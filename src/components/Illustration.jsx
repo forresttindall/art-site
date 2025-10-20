3008a71e88914a05d6896af5a192c493627fd8de
forresttindall <forrest.tindall@gmail.com>
2025-10-20 14:31:42 -0600

    liminal

--- src/components/Illustration.jsx
+++ src/components/Illustration.jsx
@@ -7,7 +7,7 @@ const Illustration = () => {
         <h1>Illustration</h1>
 
    
-        
+     {/*  
     <div className='project'>
           <div className="featured-image">
             <img src="/images/IMG_2202.PNG" alt="Featured artwork" />
@@ -22,7 +22,25 @@ const Illustration = () => {
             <span className="bold">DATE:</span> 10.13.2025
           </p>
         </div>
+*/}  
 
+
+   <div className='project'>
+          <div className="featured-image">
+            <img src="/images/beer-3.jpg" alt="Featured artwork" />
+          </div>
+          <p className="mono-text">
+            <span className="bold">Title:</span> Coyote Howls
+          </p>
+          <p className="mono-text">
+            <span className="bold">medium:</span> Digital
+          </p>
+          <p className="mono-text">
+            <span className="bold">DATE:</span> 12.29.2023
+          </p>
+        </div>
+
+        
 
            <div className='project'>
           <div className="featured-image">
@@ -40,20 +58,7 @@ const Illustration = () => {
         </div>
 
 
-        <div className='project'>
-          <div className="featured-image">
-            <img src="/images/beer-3.jpg" alt="Featured artwork" />
-          </div>
-          <p className="mono-text">
-            <span className="bold">Title:</span> Coyote Howls
-          </p>
-          <p className="mono-text">
-            <span className="bold">medium:</span> Digital
-          </p>
-          <p className="mono-text">
-            <span className="bold">DATE:</span> 12.29.2023
-          </p>
-        </div>
+     
 
         <div className='project'>
           <div className="featured-image">
